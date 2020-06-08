8c9d399f88e2e9f431638221eee3b8acd28384a0
alexanderastoray <47002689+alexanderastoray@users.noreply.github.com>
2020-06-08 08:38:14 +0200

    Add Solution services

---
+++ ServiceA/Controllers/ServiceTestAController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceA.Controllers
+{
+    [Route("api/ServiceA")]
+    [ApiController]
+    public class ServiceTestAController : ControllerBase
+    {
+    
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+        return new string[] { "DaemonService" , "TaskService" ,"ExecutionService" };
+        }
+    }
+}
